8a889d171fc61be35d6da27d6cd7ac35b3ee89af
achboune <aachboune20744@iesjoanramis.org>
2023-11-15 10:57:35 +0200

    Ex12.py

--- Ex12.py
+++ Ex12.py
@@ -1,3 +1,13 @@
+def bintodec(N):
+    a=int(N,2)
+    return dectooct (a)
+def bintooct(N):
+    a=int(N,2)
+    return oct (a)
+def bintohex(N):
+    a=int(N,2)
+    return hex (N)
+
 def calculadora_enters():
     y = 1
     while y>0:
@@ -39,12 +49,51 @@ def canvis_de_base():
                 4.Hexadecimal
                 5.Sortir
             """) 
-        
+        def canvis_de_base():
+            x = 1
+            while x>0:
+                print("""
+                Menú canis de base
+                1.Donat un binari ho passem a tota la resta
+                2.Donat un octal el passem a tota la resta
+                3.Donat decimal ho passem a tota la resta 
+                4.donat un hexadecimal ho passem ha tota la resta
+                5.Sortir               
+                """)
+                y= int(input("Introdueix el número Binario: "))   
+            if y:
+                b = input("Introdueix el número bianri")
+                o = bintooct(b)
+                d = bintodec(b)
+                h = bintohex(b)
+                print("El número binari {} és: \n oct -> {} \n dec -> {} \n hex -> {}".format(b,o,d,h))
+            elif 2: #Octal 
+                o = input("Introdueix el número octal: ")
+                b = octtobin(o)
+                d = octtodec(o)
+                h = octtohex(o)
+                print("El número octal {} és: \n bin -> {} \n dec -> {} \n hex -> {}".format(o,b,d,h))
+            elif 3: #Decimal
+                d = int(input("Introdueix el número decimal"))
+                b = dectobin(d)
+                o = dectooct(d)
+                h = dectohex(d)
+                print("Introdueix decimal {} és \n bin -> {} \n oct -> {} \n hex -> {}".format(d,b,o,h))
+            elif 4: #Hexadecimal
+                h = input("Introdueix el número hexadecimal")
+                b = hextobin(h)
+                o = hextooct(h)
+                d = hexttodec(h)
+                print("El número hexadecimal {} és: \n bin -> {} \n oct -> {} \n dec -> {}".format(h,b,o,d))
+            elif 5: #Sortir
+                print("Adéu, tornaràs a la calculadora inicial \n\n")
+                y=-1
 
         #Programa canvis de base 
         s = int(input("Elige una opción: "))
         
-        if s == 1: #Binari
+        if s == 1: 
+            
             numero = int(input("Introduce el primer numero:\n "))
             convertido = bin(numero)
             print(convertido)
